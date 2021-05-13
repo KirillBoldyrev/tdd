8f301825429e55351b99c1d12f547c4e24a934e7
Kirill Boldyrev <bke@skbkontur.ru>
2021-05-13 16:20:04 +0500

    Add CalculateScore_SimpleRollsAndSpares() test

--- cs/BowlingGame/Game.cs
+++ cs/BowlingGame/Game.cs
@@ -43,5 +43,27 @@ namespace BowlingGame
                 .GetScore()
                 .Should().Be(8);
         }
+
+        [Test]
+        public void CalculateScore_SimpleRollsAndSpares()
+        {
+            var game = new Game();
+
+            // #1 frame - simple roll
+            game.Roll(4);
+            game.Roll(4);
+
+            // #2 frame - spare 
+            game.Roll(2);
+            game.Roll(8);
+
+            // #3 frame - simple roll
+            game.Roll(3);
+            game.Roll(2);
+
+            game
+                .GetScore()
+                .Should().Be(8+10+3+5);
+        }
     }
 }
